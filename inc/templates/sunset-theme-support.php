6623dec90af57136f781168c1c2d87fd8098dcea
Hodga <hojajepbar@gmail.com>
2019-01-30 14:22:59 +0200

    Lesson64

--- inc/templates/sunset-theme-support.php
+++ inc/templates/sunset-theme-support.php
@@ -2,7 +2,7 @@
 <?php settings_errors(); ?>
 
 <form method="post" action="options.php" class="sunset-general-form">
-    <?php settings_fields( 'sunset-theme-support' ); ?>
-    <?php do_settings_sections( 'alecaddd_sunset_theme' ); ?>
-    <?php submit_button(); ?>
+	<?php settings_fields( 'sunset-theme-support' ); ?>
+	<?php do_settings_sections( 'alecaddd_sunset_theme' ); ?>
+	<?php submit_button(); ?>
 </form>
